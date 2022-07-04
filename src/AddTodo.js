3c708734bd26da3b21d32e3e820ff6570a8e3bb0
Apollostowel <apollostowel@icloud.com>
2022-07-04 18:33:07 +0300

    Initial commit
    Simple todo app exercise in React

---
+++ src/AddTodo.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+function AddTodo(props) {
+    const [open, setOpen] = useState(false);
+    const [todo, setTodo] = useState({description: '', date: '', priority: ''});
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleSave = () => {
+        props.addTodo(todo);
+        handleClose();
+    }
+
+    const inputChanged = (event) => {
+        setTodo({...todo, [event.target.name]: event.target.value});
+    }
+
+    return(
+        <div>
+            <Button variant="outlined" color="primary" onClick={handleOpen}>Add Todo</Button>
+            <Dialog open={open}>
+                <DialogTitle>New Todo</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        name="description"
+                        value={todo.description}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Description"
+                        fullWidth 
+                    />
+                    <TextField
+                        name="date"
+                        value={todo.date}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Date"
+                        fullWidth 
+                    />
+                    <TextField
+                        name="priority"
+                        value={todo.priority}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Priority"
+                        fullWidth
+                         />
+                </DialogContent>
+                <DialogActions>
+                    <Button color="primary" onClick={handleClose}>Cancle</Button>
+                    <Button color="primary" onClick={handleSave}>Save</Button>
+                </DialogActions>
+            </Dialog>
+
+        </div>
+    );
+}
+
+export default AddTodo;
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddTodo from './AddTodo';
+
+
+import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchItems(); 
+
+    
+  }, [])
+
+  const fetchItems = () => {
+    fetch('https://todolist-4e3e3-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
+    .then(response => response.json())
+    .then(data => addKeys(data))
+    .catch(err => console.error(err))
+  }
+
+  const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((item, index) => 
+    Object.defineProperty(item, 'id', {value: keys[index]}));
+    setTodos(valueKeys);
+  }
+
+  const addTodo =(newTodo) => {
+    fetch('https://todolist-4e3e3-default-rtdb.europe-west1.firebasedatabase.app/items/.json',
+    {
+      method: 'POST',
+      body: JSON.stringify(newTodo)
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
+
+  const deleteTodo = (id) => {
+    fetch(`https://todolist-4e3e3-default-rtdb.europe-west1.firebasedatabase.app/items/${id}.json`,
+    {
+      method: 'DELETE',
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
+  
+  return (
+    <div className="App">
+      <AppBar position="static" style={{marginBottom: 15}}>
+        <Toolbar>
+          <Typography variant="h5" noWrap>TodoList</Typography>
+        </Toolbar>
+      </AppBar>
+      <AddTodo addTodo={addTodo} />
+      <div className="ag-theme-material" style={{height: 400, width: 700, margin: 'auto'}}>
+        <AgGridReact rowData={todos} animateRows={true} rowDragManaged={true}>
+          <AgGridColumn sortable={true} filter={true} rowDrag={true} field='description' />
+          <AgGridColumn sortable={true} filter={true} field='date' />
+          <AgGridColumn sortable={true} filter={true} field='priority' />
+          <AgGridColumn headerName='' field='id' width={90}
+            cellRendererFramework={ params =>
+            <IconButton onClick={() => deleteTodo(params.value)} size="small" color="secondary">
+              <DeleteIcon />
+            </IconButton> 
+           } 
+          />
+        </AgGridReact> 
+      </div> 
+    </div>
+  );
+}
+
+export default App;
